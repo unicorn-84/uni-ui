6a3c82a9c1094dc377634b93cac00abae270823d
unicorn-84 <unicorn-84@ya.ru>
2021-01-15 09:49:52 +0300

    added p

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {
   H1,
+  P,
 } from '../../src';
 import './styles.css';
 
 export default () => (
   <>
-    <H1 className="title">Звёздная ночь освещает наши сны</H1>
+    <H1 className="title">
+      Выразительный
+      <br />
+      JAVASCRIPT
+    </H1>
+    <P className="body-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla autem labore minus facere tempore unde?</P>
   </>
 );
---
+++ src/components/atoms/P/P.component.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import './P.styles.css';
+
+export default ({ children, className }) => (<p className={className}>{children}</p>);
