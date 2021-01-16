66d56231cd46fb22939359ee720d99dc96b5fb4f
unicorn-84 <unicorn-84@ya.ru>
2021-01-16 21:47:42 +0300

    remove components

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -3,11 +3,10 @@ import {
   Title,
   Text,
 } from '../../src';
-import './styles.css';
 
 export default () => (
   <>
-    <Title>Звёздная ночь освещает наши сны</Title>
-    <Text>Звёздная ночь освещает наши сны</Text>
+    <Title>Выразительный Javascript</Title>
+    <Text>3-е издание</Text>
   </>
 );
--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,6 +1,2 @@
 export { default as Title } from './Title';
 export { default as Text } from './Text';
-export { default as H2 } from './H2';
-export { default as H3 } from './H3';
-export { default as H4 } from './H4';
-export { default as P } from './P';
