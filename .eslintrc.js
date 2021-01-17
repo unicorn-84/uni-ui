d46ab36979a897ad2296cab2af46a67a5bf5afd5
unicorn-84 <unicorn-84@ya.ru>
2021-01-18 01:02:53 +0300

    Added jsdoc comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'react',
     'jest',
     'testing-library',
+    'jsdoc',
   ],
   rules: {},
 };
--- src/components/atoms/Title/Title.component.jsx
+++ src/components/atoms/Title/Title.component.jsx
@@ -3,6 +3,14 @@ import stylePropType from 'react-style-proptype';
 import PropTypes from 'prop-types';
 import StyledTitle from './Title.styles';
 
+/**
+ * Component for showing heading text.
+ *
+ * @component
+ * @example
+ * <Title>Heading text</Title>
+ */
+
 const Title = ({
   children,
   a11yLabel,
@@ -22,8 +30,17 @@ Title.defaultProps = {
 };
 
 Title.propTypes = {
+  /**
+   * Any nodes to be rendered (usually text nodes)
+   */
   children: PropTypes.node.isRequired,
+  /**
+   * Applied as aria-label HTML attribute to be used by screen readers
+   */
   a11yLabel: PropTypes.string,
+  /**
+   * Applied as style HTML attribute that can be used to create inline css
+   */
   style: stylePropType,
 };
 
